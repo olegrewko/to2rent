2739d7c76226edfa27e3233d3fb1000c095ebc53
olegrewko <olegrewko@gmail.com>
2022-11-16 13:09:37 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider').slick({
+
+        prevArrow: '<button class ="slick-arrow slick-prev"><img src="images/sv.svg" alt="../images/sv.svg" ></button>',
+        nextArrow:'<button class ="slick-arrow slick-next"><img src="images/sp.svg" alt= "../images/sp.svg"></button>',
+        fade: true
+    });
+
+
+
+});
